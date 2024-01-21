598e5c46c30c1263051fcd884b7041b4e5f9b1ba
Darshan Shah <Darshan.Shah2@Honeywell.com>
2024-01-21 13:37:04 -0500

    Read servo angle from cmd args

--- test_scripts/test_servo_rpi.py
+++ test_scripts/test_servo_rpi.py
@@ -1,20 +1,33 @@
+import sys
 import RPi.GPIO as GPIO
 from time import sleep
 
+args = sys.argv
+
+# Check if at least two arguments are provided (including the script name)
+if len(args) >= 2:
+    pin_number = args[1]  # Get the first argument (Pin number)
+    angle = args[2]   # Get the second argument (angle)
+
+    print("Rotating Servo attached at BOARD pin: " + pin_number +" by angle : " + angle)
+else:
+    print("Please provide Pin number and angle as command-line arguments.")
+    print("Example usage: python3 test_servo_rpi.py 11 90")
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-pwm=GPIO.PWM(11, 50)
-pwm.start(0)
+GPIO.setup(pin_number, GPIO.OUT)
+pwm=GPIO.PWM(pin_number, 50)
+pwm.start(angle)
 
 def SetAngle(angle):
 	duty = angle / 18 + 2
-	GPIO.output(11, True)
+	GPIO.output(pin_number, True)
 	pwm.ChangeDutyCycle(duty)
 	sleep(1)
-	GPIO.output(11, False)
+	GPIO.output(pin_number, False)
 	pwm.ChangeDutyCycle(0)
 
-SetAngle(90)
+SetAngle(angle)
 pwm.stop()
 
 GPIO.cleanup()
