38e2a1612cb8fabf0fa76e2952b86bed9c35a231
Darshan Shah <dshah003@terpmail.umd.edu>
2024-01-21 13:57:08 -0500

    Convert args to int

--- test_scripts/test_servo_rpi.py
+++ test_scripts/test_servo_rpi.py
@@ -6,10 +6,10 @@ args = sys.argv
 
 # Check if at least two arguments are provided (including the script name)
 if len(args) >= 2:
-    pin_number = args[1]  # Get the first argument (Pin number)
-    angle = args[2]   # Get the second argument (angle)
+    pin_number = int(args[1])  # Get the first argument (Pin number)
+    angle = int(args[2])   # Get the second argument (angle)
 
-    print("Rotating Servo attached at BOARD pin: " + pin_number +" by angle : " + angle)
+    print("Rotating Servo attached at BOARD pin: " + str(pin_number) +" by angle : " + str(angle))
 else:
     print("Please provide Pin number and angle as command-line arguments.")
     print("Example usage: python3 test_servo_rpi.py 11 90")
@@ -17,7 +17,7 @@ else:
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin_number, GPIO.OUT)
 pwm=GPIO.PWM(pin_number, 50)
-pwm.start(angle)
+pwm.start(7.5)
 
 def SetAngle(angle):
 	duty = angle / 18 + 2
